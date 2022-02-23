c8c0d08b47044349b29fc0cb40e014f7aec27dcc
Henry Miller <mail.henrym+git@gmail.com>
2022-02-23 10:37:12 -0500

    commit tree 3f27cb81b42d9af2596dc48f6f6bac688390d748

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
-# Your code goes here!
+# class Anagram
+#     attr_reader :word
+
+#     def initialize(word)
+#         @word 
+#     end 
+
+#     def match(array)
+#         array.select {|word2| word2.chars.sort==word.chars.sort}
+#     end
+
+# end 
+
+class Anagram
+
+    attr_accessor :name
+  
+    def initialize(word)
+      @name = word 
+    end   
+  
+    def match(array)
+      array.select {|x| x.split("").sort == @name.split("").sort}
+    end   
+  
+  end 
